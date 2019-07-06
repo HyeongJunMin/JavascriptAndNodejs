29f7cdd5525f5c76a0381fbdf915ba118e3d4d91
hyeongjunmin <alsgudwns007@naver.com>
2019-07-06 16:54:47 +0900

    2019-07-04 Nodejs

---
+++ Syntax/array/array.js
@@ -0,0 +1,6 @@
+var arr = ['a','b','c','d'];
+console.log(arr[2]);
+console.log(arr);
+console.log(arr.length);
+arr.push('e');
+console.log(arr);
---
+++ Syntax/loopStatement/loop.js
@@ -0,0 +1,8 @@
+var w = 0;
+while(w < 2){
+  console.log(w++);
+}
+console.log('');
+for(w = 0; w < 4; w++){
+  console.log(w);
+}
