5cfab0490cdee6a394bfcb3cdf2a5c0824468be9
santaMin_yeBbi <alsgudwns007@naver.com>
2020-01-05 19:04:28 +0900

    db연결 성공

--- react-exam1/src/board/list.js
+++ react-exam1/src/board/list.js
@@ -1,18 +1,52 @@
 import React, {Component} from "react";
 
 class BoardList extends Component {
-    state = {username: null}
+    state = {username: null,
+            list: [
+                // {id:1, content:'1111'},
+                // {id:2, content:'2222'}
+          ],
+        info: [
+            {id:1, content:'1111'},
+            {id:2, content:'2222'}
+        ]
+    }
 
     componentDidMount() {
-        fetch('http://localhost:2000/api/board/list')
+        fetch('http://localhost:2000/api/board/connectioncheck')
             .then(res => res.json())
-            .then(user => this.setState({ username: user.data }));
+            .then(user => this.setState({ username: user.data }))
+            .then(json => console.log(json))
+
+        fetch('http://localhost:2000/api/board/list')
+            .then(data => {this.setState({list: data.list})})
+            .then(data => console.log(data))
+            .then(data => console.log(JSON.stringify(data)))
+
+        // this._getBoardList();
+        this._callApi()
+            .then(res => this.setState({info: res.list}))
+            .catch(err => console.log(err));
+    }
+
+    _getBoardList = async () => {
+        const boardList = await this._callApi();
+        this.setState({info: boardList});
+    }
+
+    _callApi = async () => {
+        const resp = await fetch("http://localhost:2000/api/board/list")
+        const body = (await resp).json();
+        return body;
     }
 
     render() {
         return (
             <div className="BoardList">
                 {this.state.username ? <h1>{`Hello ${this.state.username}`}</h1> : <h1>Loading.. please wait!</h1>}
+                {this.state.list}
+                {JSON.stringify(this.state.info)}
+
             </div>
         );
     }
--- react-exam1/src/board/main.js
+++ react-exam1/src/board/main.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import List from './list';
+import GetJsonArr from "../exams/getJsonArr";
 
 function BoardMain() {
     return (
         <div className="BoardMain">
-            <p>게시판 메인페이지</p>
+            <p>게시판 연습 메인페이지</p>
             <List></List>
+
+            
+            <h2>서버로부터 json타입 배열 받아오는 방법 정리</h2>
+            <GetJsonArr></GetJsonArr>
         </div>
     );
 }
